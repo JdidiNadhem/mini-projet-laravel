5ddc9a03c4501abd50095c3b7fba2f5abc16aaa1
JdidiNadhem <jedidinadhem@gmail.com>
2024-01-08 00:05:50 +0100

    first adding

---
+++ resources/views/dates.blade.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Jurse2025</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Variables CSS Files. Uncomment your preferred color scheme -->
+    <link href="assets/css/variables.css" rel="stylesheet">
+    <!-- <link href="assets/css/variables-blue.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/variables-green.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/variables-orange.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/variables-purple.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/variables-red.css" rel="stylesheet"> -->
+    <!-- <link href="assets/css/variables-pink.css" rel="stylesheet"> -->
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: HeroBiz
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="header fixed-top" data-scrollto-offset="0">
+        <div class="container-fluid d-flex align-items-center justify-content-between">
+
+            <a href="/" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+                <!-- Uncomment the line below if you also wish to use an image logo -->
+                <!-- <img src="assets/img/logo.png" alt=""> -->
+                <h1>Jurse2025<span></span></h1>
+            </a>
+
+            <nav id="navbar" class="navbar">
+                <ul>
+
+                    <li><a class="nav-link scrollto" href="/">Home</a></li>
+                    <li><a class="nav-link scrollto" href="dates">Dates</a></li>
+                    <li class="dropdown"><a href="participation"><span>Participation</span> <i
+                                class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="/">PROGRAM</a></li>
+                            <li><a href="/">GUIDELINES</a></li>
+                            <li><a href="/">CALL FOR PAPERS</a></li>
+                            <li><a href="/">REGISTRATION</a></li>
+                            <li><a href="/">SOCIAL EVENTS</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="/"><span>Présentation</span> <i
+                                class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="/">KEYNOTES</a></li>
+                            <li><a href="/">TOPICS</a></li>
+                            <li><a href="/">SPECIAL SESSIONS</a></li>
+                            <li><a href="/">TECHNICAL TUTORIALS</a></li>
+                            <li><a href="/">STUDENT CONTEST</a></li>
+                            <li><a href="/">EXHIBITORS</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="index.html#portfolio">Committees</a></li>
+                    <li><a class="nav-link scrollto" href="index.html#team">Sponsors</a></li>
+                    <li class="dropdown"><a href="/"><span>Venue</span> <i
+                                class="bi bi-chevron-down dropdown-indicator"></i></a>
+                        <ul>
+                            <li><a href="/">HERAKILTON & CRETE</a></li>
+                            <li><a href="/">CONFERENCE VENUE</a></li>
+                            <li><a href="/">HOTELS</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a class="nav-link scrollto" href="index.html#team">Video & Photo</a></li>
+                    <li><a class="nav-link scrollto" href="index.html#team">Contact</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle d-none"></i>
+            </nav><!-- .navbar -->
+
+        </div>
+    </header><!-- End Header -->
+    <main>
+        <section id="dates-list">
+            <div class="row mb-5">
+                <div class="col-12" id="line-title">
+                    <h3>Dates</h3>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <table class="table table-striped table-bordered">
+                            <thead>
+                                <tr>
+                                    <th class="w-50">Action</th>
+                                    <th class="w-50">Deadline</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Special Sessions Proposal Deadline</td>
+                                    <td>30 June 2022</td>
+                                </tr>
+                                <tr>
+                                    <td>Open submission Website</td>
+                                    <td>7 September 2022</td>
+                                </tr>
+                                <tr>
+                                    <td>Papers Submission Deadline</td>
+                                    <td>9 December 2022 (updated)</td>
+                                </tr>
+                                <tr>
+                                    <td>Acceptance Notification</td>
+                                    <td>31 January 2023</td>
+                                </tr>
+                                <tr>
+                                    <td>Open Registration Website</td>
+                                    <td>31 January 2023</td>
+                                </tr>
+                                <tr>
+                                    <td>Early Registration Deadline</td>
+                                    <td>31 March 2023</td>
+                                </tr>
+                                <tr>
+                                    <td>Camera-ready Deadline</td>
+                                    <td>7 April 2023</td>
+                                </tr>
+                                <tr>
+                                    <td>Satellite Events</td>
+                                    <td>16 May 2023</td>
+                                </tr>
+                                <tr>
+                                    <td>Main Conference</td>
+                                    <td>17 - 19 May 2023</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+
+        <div class="footer-content">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-4">
+                        <div class="footer-info text-center">
+                            <h3>Stay connected!</h3>
+                            <div class="footer-legal" style="background: transparent;">
+                                <div class="social-links order-first order-lg-last mb-3 mb-lg-0">
+                                    <a href="/" class="twitter"><i class="bi bi-twitter"></i></a>
+                                    <a href="/" class="facebook"><i class="bi bi-facebook"></i></a>
+                                    <a href="/" class="instagram"><i class="bi bi-instagram"></i></a>
+                                    <a href="/" class="google-plus"><i class="bi bi-skype"></i></a>
+                                    <a href="/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4">
+                        <div class="footer-info text-center">
+                            <h3>Contact person</h3>
+                            <ul id="contact">
+                                <li>Nektarios Chrysoulakis</li>
+                                <li>zedd2@iacm.forth.gr</li>
+                                <li>+30 2810-391-762</li>
+                                <li>+30 6932-929-775</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-4">
+                        <div class="footer-info text-center">
+                            <h3>Venu</h3>
+                            <ul id="contact">
+                                <li>Cultural Conference Center of Heraklion</li>
+                                <li>N. Plastira Avenue 49, Heraklion, 71201 Greece</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-legal text-center">
+            <div
+                class="container d-flex flex-column flex-lg-row justify-content-center justify-content-lg-between align-items-center">
+
+                <div class="d-flex flex-column align-items-center align-items-lg-start">
+                    <div class="copyright">
+                        © Copyright 2024: Remote Sensing Laboratory, Foundation for Research and Technology - Hellas
+                        (FORTH)
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer><!-- End Footer -->
+
+    <a href="/" class="scroll-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <div id="preloader"></div>
+
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
